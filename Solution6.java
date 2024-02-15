6aab2d9f27a3a2e11b31aa8616a69b5b6698cf2d
pjh753156a <158389187+pjh753156a@users.noreply.github.com>
2024-02-15 12:34:01 +0900

    Add files via upload

---
+++ Solution6.java
@@ -0,0 +1,32 @@
+package print;
+
+/*
+ * 문자열 my_string과 정수 배열 index_list가 
+ * 매개변수로 주어집니다. 
+ * my_string의 index_list의 원소들에 해당하는 인덱스의 글자들을 
+ * 순서대로 이어 붙인 문자열을 return 하는 
+ * solution 함수를 작성해 주세요.
+ */
+
+public class Solution6 
+{
+	
+	public static String solution(String my_string, int[] index_list) 
+	{
+        String answer = "";
+        for(int i=0; i<index_list.length;i++)
+        {
+        	answer=answer+my_string.charAt(index_list[i]);
+        }
+        return answer;
+    }
+	
+	public static void main(String[] args)
+	{
+		String my_string = "cvsgiorszzzmrpaqpe";
+		int index_list[] = {16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7};
+		String answer = solution(my_string,index_list);
+		System.out.println(answer);
+	}
+
+}
