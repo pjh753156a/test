002db2fadb13694066b5ab32de86c1ebe9227d4f
pjh753156a <158389187+pjh753156a@users.noreply.github.com>
2024-03-23 23:27:47 +0900

    Add files via upload

---
+++ Solution88.java
@@ -0,0 +1,36 @@
+package lv00;
+
+import java.util.Arrays;
+
+/*
+ * 정수 n이 매개변수로 주어질 때, 
+ * 다음과 같은 n × n 크기의 이차원 배열 arr를 return 하는 
+ * solution 함수를 작성해 주세요.
+ * arr[i][j] (0 ≤ i, j < n)의 값은 
+ * i = j라면 1, 아니라면 0입니다.
+ */
+
+public class Solution88 
+{
+	public static int[][] solution(int n) 
+	{
+        int[][] answer = {};
+        answer = new int[n][n];
+        for(int i=0;i<answer.length;i++)
+        {
+        	answer[i][i]=1;
+        }
+        return answer;
+    }
+	
+	public static void main(String[] args) 
+	{
+		int n = 1;
+		int[][] result = solution(n);
+		
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(Arrays.toString(result[i]));
+		}
+ 	}
+}
