dc7d70e589eb1bd44d4c2b9aa7ef4c8e9eb1f528
pjh753156a <158389187+pjh753156a@users.noreply.github.com>
2024-03-26 21:20:32 +0900

    Add files via upload

--- Solution100.java
+++ Solution100.java
@@ -3,7 +3,7 @@ package Iv000;
 /*
  * 2차원 정수 배열 board와 정수 k가 주어집니다.
  * i + j <= k를 만족하는 모든 (i, j)에 대한 board[i][j]의 합을 return 하는 
- * solution 함수를 완성해 주세요.
+ * solution 함수를 완성해 주세요.100!!!
  */
 
 public class Solution100 
