7bb0a565ab45377f75a937f040b18d072115ae88
pjh753156a <158389187+pjh753156a@users.noreply.github.com>
2024-03-26 16:01:12 +0900

    Add files via upload

---
+++ Solution98.java
@@ -0,0 +1,25 @@
+package programmers;
+
+/*
+ * 정수 num1과 num2가 매개변수로 주어질 때, 
+ * num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 
+ * soltuion 함수를 완성해주세요.
+ */
+
+public class Solution98 
+{
+	public static int solution(int num1, int num2) 
+	{	        
+		int answer = 0;
+	    answer = (int)((double)num1/num2*1000);
+		return answer;
+	}
+	
+	public static void main(String[] args)
+	{
+		int num1=7;
+		int num2=3;
+		int result = solution(num1, num2);
+		System.out.println(result);
+	}
+}
