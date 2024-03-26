f04a37b8e6ed9b961fcaebea3733863dd12db557
pjh753156a <158389187+pjh753156a@users.noreply.github.com>
2024-03-26 21:16:53 +0900

    Add files via upload

--- Solution92.java
+++ Solution92.java
@@ -1,32 +1,33 @@
-package lv00;
+package Iv000;
 
 /*
- * 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, 
- * array에 n이 몇 개 있는 지를 return 하도록 
- * solution 함수를 완성해보세요.
+ * 2차원 정수 배열 board와 정수 k가 주어집니다.
+ * i + j <= k를 만족하는 모든 (i, j)에 대한 board[i][j]의 합을 return 하는 
+ * solution 함수를 완성해 주세요.
  */
 
 public class Solution92 
 {
-	public static int solution(int[] array, int n) 
+	public static int solution(int[][] board, int k) 
 	{
-        int answer = 0;
-        for(int i=0;i<array.length;i++)
-        {
-        	if(array[i]==n)
-        	{
-        		answer++;
-        	}
-        }
-        return answer;
-    }
-
+	        int answer = 0;
+	        for(int i=0;i<board.length;i++)
+	        {
+	        	for(int j=0;j<board[i].length;j++)
+	        	{
+	        		if(i+j<=k)
+	        		{
+	        			answer=answer+board[i][j];
+	        		}
+	        	}
+	        }
+	        return answer;
+	}
 	public static void main(String[] args) 
 	{
-		int[] array = {1, 1, 2, 3, 4, 5};
-		int n=1;
-		int result = solution(array, n);
+		int[][] board = {{0, 1, 2},{1, 2, 3},{2, 3, 4},{3, 4, 5}};
+		int k=2;
+		int result = solution(board, k);
 		System.out.println(result);
 	}
-
 }
